2f3508b454ecc9cc38abbf49a4e8a46b203bfe45
sachinvectoriq <sbhusanurmath@vectoriq.ai>
2025-06-06 10:14:00 +0530

    Update search_query.py

--- search_query.py
+++ search_query.py
@@ -35,7 +35,7 @@ def search_and_answer_query(user_query, user_id):
     token_provider = get_bearer_token_provider(credential, "https://cognitiveservices.azure.com/.default")
 
     AZURE_SEARCH_SERVICE = "https://aiconciergeserach.search.windows.net"
-    index_name = "Index-peoplesoft"
+    index_name = "index-peoplesoft"
     deployment_name = "ocm-gpt-4o"
 
     openai_client = AzureOpenAI(
